9e99e176189872392b42f6955df97770b227d55f
danielcs7 <danielcarloss7@icloud.com>
2019-04-11 19:36:34 -0300

    troca do url2

--- src/server.js
+++ src/server.js
@@ -37,4 +37,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(require("./routes"));
+
+//para reconhecer o horuku
 server.listen(process.env.PORT || 3000);
